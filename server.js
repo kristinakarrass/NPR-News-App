b1369aca14cf01bc227060e8934a20aa87151346
Kristina Karrass <kristinakarrass@gmail.com>
2017-06-27 11:56:59 -0400

    scrape

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var cheerio = require("cheerio");
 var request = require("request");
 
 //Set mongoose to leverage built in JavaScript ES6 Promises
-mongoose.Promise=Promise;
+mongoose.Promise = Promise;
 
 //set port to 3000 or whichever one is used in environment variable
 var PORT = process.env.PORT || 3000;
@@ -17,13 +17,13 @@ var app = express();
 
 //Set the app up with body-parser and a static folder
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 //serve static content for the app from the "public" directory in the application directory
 app.use(express.static("public"));
 
 //handlebars setup as view engine
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 //Database configuration with mongoose
@@ -32,20 +32,53 @@ var db = mongoose.connection;
 
 //Show any mongoose errors
 db.on("error", function(error) {
-	console.log("Mongoose Error: ", error);
+    console.log("Mongoose Error: ", error);
 });
 
 //Once logged in to the db through mongoose, log a success message
 db.once("open", function() {
-	console.log("Mongoose connection successful.");
+    console.log("Mongoose connection successful.");
 });
 
-//Simple index route
-app.get("/", function(req, res) {
-	res.send(index.html);
-});
+//Routes
+//=======================================================================================
+
+//A GET request to scrape the NPR.org website for newsarticles
+app.get("/scrape", function(req, res) {
+	//First, we grab the body of the html with request
+    request("http://www.npr.org/sections/news/", function(error, response, html) {
+    	//Then we load it into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(html);
+        //Grab every item.has-image and get their title, link, teaser text and image link
+        $(".item.has-image").each(function(i, element) {
+        	//save and empty result object
+        	var result = {};
+            //add selected information for each article as properties of the results object
+            result.title = $(this).children(".item-info").find("h2.title").text();
+            result.link = $(this).children(".item-info").find("h2.title").find("a").attr("href");
+            result.teaser = $(this).children(".item-info").find("p.teaser").text();
+            result.imgLink = $(this).children(".item-image").find("a").find("img").attr("src");
+            //using our Article model, cerate a new entry
+            //this passes the result object to the entry
+            var entry = new Article(result);
+            //save that entry to the db
+            entry.save(function(err, doc) {
+            	//log any errors
+            	if (err) {
+            		console.log(err);
+            	}
+            	//or log the doc
+            	else {
+            		console.log(doc);
+            	}
+            });
+        });
+    });
+    //tell user scrape is completed
+    res.send("scrape complete"); //make this a modal with handlebars and send it to HTML
+})
 
 // Listen on PORT
 app.listen(PORT, function() {
-  console.log("App running on port 3000!");
+    console.log("App running on port 3000!");
 });
