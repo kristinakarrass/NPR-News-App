1e7289592c9de8d9da99fdab137bee4b0241ad6b
Kristina Karrass <kristinakarrass@gmail.com>
2017-06-27 11:30:41 -0400

    setup

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var express = require("express");
 var exhbs = require("express-handlebars");
 var mongoose = require("mongoose");
 
+var PORT = process.env.PORT || 3000;
+
 var app = express();
 
 //Set the app up with body-parser and a static folder
@@ -13,7 +15,9 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-
+//handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 //Simple index route
 app.get("/", function(req, res) {
